fb4f11ea0e6fee74353a77acc5204b42b98dfbb6
2no553 <ninomiya.ryota@gmail.com>
2019-03-02 20:01:56 +0900

    puma-nginxをsrvに移行

--- custom_sample/puma.rb
+++ custom_sample/puma.rb
@@ -4,7 +4,7 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
-app_dir = "/myapp"
+app_dir = "/srv"
 shared_dir = "/puma_shared"
 bind "unix://#{app_dir}#{shared_dir}/sockets/puma.sock"
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
