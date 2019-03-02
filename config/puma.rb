5b8fd194ea336b82c74e8eff2405ffcb213fa36a
2no553 <ninomiya.ryota@gmail.com>
2019-03-02 18:35:07 +0900

    puma-nginxリファクタ

--- config/puma.rb
+++ config/puma.rb
@@ -5,8 +5,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 app_dir = "/myapp"
-shared_dir = "puma_shared"
-
-bind "unix://#{app_dir}/#{shared_dir}/sockets/puma.sock"
-
+shared_dir = "/puma_shared"
+bind "unix://#{app_dir}#{shared_dir}/sockets/puma.sock"
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
